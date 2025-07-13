9f47b239eef2d87bba13383a59efa1759fdf0892
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 01:43:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { PublicationProvider } from "./context/PublicationContext.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx"; // Import AuthProvider
+import { PublicationProvider } from "./context/PublicationsContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <AuthProvider> {/* Bungkus dengan AuthProvider */}
+      <AuthProvider>
         <PublicationProvider>
           <App />
         </PublicationProvider>
