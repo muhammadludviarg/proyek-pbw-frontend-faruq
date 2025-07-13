5ede100670116e3ed0bd4fbe47555b8a31ff0f19
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 01:39:37 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+// src/components/Footer.jsx
+
+import React from "react";
+
 
 export default function Footer() {
+
   return (
-    <footer className="text-center py-8">
-      <p className="text-gray-500 text-sm">
-        Created by Muhammad Faruq Hafidzullah Erfaringga (2223131868@stis.ac.id)
-      </p>
+    <footer className="bg-gray-100 py-8"> 
+      <div className="max-w-7xl mx-auto px-4 text-gray-700 text-center text-sm"> 
+        <p>
+          Created by Muhammad Faruq Hafidzullah Erfaringga (222313186@stis.ac.id) 
+        </p>
+      </div>
     </footer>
   );
 }
