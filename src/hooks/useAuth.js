87097a3d3d45be72f39972fbb365cdcd1f3a9806
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 01:41:48 +0700

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,9 @@
-//src/hooks/useAuth.js
+// src/hooks/useAuth.js
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  // Tambahkan console.log di sini untuk melihat status user
-  console.log('useAuth: user status', context?.user); 
   if (!context) {
     throw new Error("useAuth must be used within AuthProvider");
   }
