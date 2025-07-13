fa0599157d0cf530545dbd36b0008c5f311f4e26
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 00:22:16 +0700

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,21 +1,26 @@
 // src/services/authService.js
-import apiClient from '../api/axios';
+import apiClient from "../api/axios";
 
 export const authService = {
   async login(email, password) {
     try {
-      const response = await apiClient.post('/login', { email, password });
+      const response = await apiClient.post("/login", { email, password });
       return response.data;
     } catch (error) {
-      throw new Error('Gagal login: ' + error.response?.data?.message || 'Terjadi kesalahan');
+      throw new Error(
+        "Gagal login: " + error.response?.data?.message || "Terjadi kesalahan"
+      );
     }
   },
+
   async logout() {
     try {
-      const response = await apiClient.post('/logout'); // Panggil endpoint logout di backend
+      const response = await apiClient.post("/logout");
       return response.data;
     } catch (error) {
-      throw new Error('Gagal logout: ' + error.response?.data?.message || 'Terjadi kesalahan');
+      throw new Error(
+        "Gagal logout: " + error.response?.data?.message || "Terjadi kesalahan"
+      );
     }
-  }
-}
+  },
+};
